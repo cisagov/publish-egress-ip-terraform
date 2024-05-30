a9feac58e1a97bbcca1e73156eacf47893e10408
dav3r <david.redmin@gwe.cisa.dhs.gov>
2024-05-30 10:26:37 -0400

    Move TF configuration blocks to be after TF arguments in resources
    This is a recently agreed upon change to how our team should format Terraform.
    
    Co-authored-by: Nick <50747025+mcdonnnj@users.noreply.github.com>

--- route53.tf
+++ route53.tf
@@ -5,25 +5,27 @@
 resource "aws_route53_record" "rules_vm_A" {
   provider = aws.route53resourcechange
 
+  name    = var.domain
+  type    = "A"
+  zone_id = data.terraform_remote_state.dns_cyber_dhs_gov.outputs.cyber_dhs_gov_zone.id
+
   alias {
     evaluate_target_health = false
     name                   = aws_cloudfront_distribution.egress_info.domain_name
     zone_id                = local.cloudfront_zone_id
   }
-  name    = var.domain
-  type    = "A"
-  zone_id = data.terraform_remote_state.dns_cyber_dhs_gov.outputs.cyber_dhs_gov_zone.id
 }
 
 resource "aws_route53_record" "rules_vm_AAAA" {
   provider = aws.route53resourcechange
 
+  name    = var.domain
+  type    = "AAAA"
+  zone_id = data.terraform_remote_state.dns_cyber_dhs_gov.outputs.cyber_dhs_gov_zone.id
+
   alias {
     evaluate_target_health = false
     name                   = aws_cloudfront_distribution.egress_info.domain_name
     zone_id                = local.cloudfront_zone_id
   }
-  name    = var.domain
-  type    = "AAAA"
-  zone_id = data.terraform_remote_state.dns_cyber_dhs_gov.outputs.cyber_dhs_gov_zone.id
 }
